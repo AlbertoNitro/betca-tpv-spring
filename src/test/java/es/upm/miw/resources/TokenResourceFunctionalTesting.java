9aaea0763e733fe3e0276704a317e26be39f1150
jbernal <j.bernal@upm.es>
2018-01-27 17:54:45 +0100

    Resolved Bug.

--- src/test/java/es/upm/miw/resources/TokenResourceFunctionalTesting.java
+++ src/test/java/es/upm/miw/resources/TokenResourceFunctionalTesting.java
@@ -35,13 +35,13 @@ public class TokenResourceFunctionalTesting {
     @Test
     public void testLoginAdminUnauthorized() {
         thrown.expect(new HttpMatcher(HttpStatus.UNAUTHORIZED));
-        restService.logout().restBuilder().path(TokenResource.TOKENS).basicAuth(this.restService.getAdminMobile(), "kk").post().build();
+        restService.restBuilder().path(TokenResource.TOKENS).basicAuth(this.restService.getAdminMobile(), "kk").post().build();
     }
 
     @Test
     public void testLoginUnauthorized() {
         thrown.expect(new HttpMatcher(HttpStatus.UNAUTHORIZED));
-        restService.logout().restBuilder().path(TokenResource.TOKENS).basicAuth("kk", "kk").post().build();
+        restService.restBuilder().path(TokenResource.TOKENS).basicAuth("kk", "kk").post().build();
     }
 
     @Test 
