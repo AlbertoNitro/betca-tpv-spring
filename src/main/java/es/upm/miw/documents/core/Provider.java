b32727472267d12ba32c7e274196079e1a0e0803
jbernal <j.bernal@upm.es>
2018-01-28 13:52:26 +0100

    add Article & provider

---
+++ src/main/java/es/upm/miw/documents/core/Provider.java
@@ -0,0 +1,106 @@
+package es.upm.miw.documents.core;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Provider {
+
+    @Id
+    private String id;
+
+    private String company;
+
+    private String address;
+
+    private long mobile;
+
+    private long phone;
+
+    private String note;
+
+    public Provider() {
+    }
+
+    public Provider(String company, String address, long mobile, long phone, String note) {
+        this.company = company;
+        this.address = address;
+        this.mobile = mobile;
+        this.phone = phone;
+        this.note = note;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(long mobile) {
+        this.mobile = mobile;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        return id == ((Provider) obj).id;
+    }
+
+    @Override
+    public String toString() {
+        return "Provider[" + id + ": company=" + company + ", address=" + address + ", mobile=" + mobile + ", phone=" + phone + ", note="
+                + note + "]";
+    }
+
+}
