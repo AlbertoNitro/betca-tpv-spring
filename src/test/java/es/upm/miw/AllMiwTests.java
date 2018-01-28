7a400184065ca58f83ba58810e5acd56f631a9f2
jbernal <j.bernal@upm.es>
2018-01-28 12:44:59 +0100

    docs

--- src/test/java/es/upm/miw/AllMiwTests.java
+++ src/test/java/es/upm/miw/AllMiwTests.java
@@ -7,15 +7,19 @@ import org.junit.runners.Suite.SuiteClasses;
 import es.upm.miw.documents.users.AllMiwDocumentsUsersTests;
 import es.upm.miw.repositories.users.AllMiwRepositoriesUsersIntegrationTests;
 import es.upm.miw.resources.AllMiwResourcesFunctionalTests;
+import es.upm.miw.services.DatabaseSeederServiceIT;
 
 @RunWith(Suite.class)
 @SuiteClasses({
     ApplicationTest.class,
-    ApplicationIT.class,
-    ApplicationFunctionalTesting.class,
     AllMiwDocumentsUsersTests.class,
+    
     AllMiwRepositoriesUsersIntegrationTests.class,
-    AllMiwResourcesFunctionalTests.class
+    ApplicationIT.class,
+    
+    AllMiwResourcesFunctionalTests.class,
+    ApplicationFunctionalTesting.class,
+    DatabaseSeederServiceIT.class
 })
 public class AllMiwTests {
 }
