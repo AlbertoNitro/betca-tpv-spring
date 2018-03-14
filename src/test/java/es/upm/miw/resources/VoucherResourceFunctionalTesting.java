fa14b0955724fe98cf8a5c96c68856823d728fbb
angel <bo0477@192.168.7.19>
2018-03-14 15:30:11 +0100

    Init tarea 5

---
+++ src/test/java/es/upm/miw/resources/VoucherResourceFunctionalTesting.java
@@ -0,0 +1,18 @@
+package es.upm.miw.resources;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class VoucherResourceFunctionalTesting {
+	
+	@Autowired
+    private RestService restService;
+	
+	
+
+}
