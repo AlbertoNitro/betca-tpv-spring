60f02228da4f821f1025632f873313530fcdf830
angel <bo0477@192.168.7.19>
2018-03-15 10:08:50 +0100

    VoucherReferenceNotFound Exception

--- src/main/java/es/upm/miw/resources/exceptions/VoucherReferenceNotFoundException.java
+++ src/main/java/es/upm/miw/resources/exceptions/VoucherReferenceNotFoundException.java
@@ -1,5 +1,17 @@
 package es.upm.miw.resources.exceptions;
 
-public class VoucherReferenceNotFoundException {
+public class VoucherReferenceNotFoundException extends Exception {
+	
+	private static final long serialVersionUID = 6546553369067053261L;
+	
+	public static final String DESCRIPTION = "Voucher reference not found";
 
+    public VoucherReferenceNotFoundException() {
+        super(DESCRIPTION);
+    }
+
+    public VoucherReferenceNotFoundException(String detail) {
+        super(DESCRIPTION + ". " + detail);
+    }
+    
 }
