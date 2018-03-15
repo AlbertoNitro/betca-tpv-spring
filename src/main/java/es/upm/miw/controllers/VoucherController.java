a1883b755109cf8203a1d6ccfd2d6e5069df0381
angel <bo0477@192.168.7.19>
2018-03-15 14:58:32 +0100

    Create and read (VoucherController)

--- src/main/java/es/upm/miw/controllers/VoucherController.java
+++ src/main/java/es/upm/miw/controllers/VoucherController.java
@@ -1,9 +1,18 @@
 package es.upm.miw.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import es.upm.miw.documents.core.Role;
+import es.upm.miw.documents.core.User;
 import es.upm.miw.documents.core.Voucher;
+import es.upm.miw.dtos.UserDto;
+import es.upm.miw.dtos.UserMinimumDto;
 import es.upm.miw.dtos.VoucherDto;
 import es.upm.miw.repositories.core.VoucherRepository;
 
@@ -14,8 +23,32 @@ public class VoucherController {
     private VoucherRepository voucherRepository;
 	
 	public void createVoucher( VoucherDto voucherDto ) {
-		
+		Voucher voucher = new Voucher( voucherDto.getValue() );
+		this.voucherRepository.save( voucher );
 	}
+	
+	public Optional<VoucherDto> readVoucher(String reference) {
+		
+		Voucher voucherBd = this.voucherRepository.findByReference( reference );
+		
+		if ( voucherBd == null ) {
+			return Optional.empty();
+		}else {
+			return Optional.of( new VoucherDto( voucherBd ) );
+		}
+    }
+	
+	public List<VoucherDto> readVoucherAll() {
+		
+		List <Voucher> voucherList = this.voucherRepository.findAll();
+		List<VoucherDto> voucherDtoList = new ArrayList<VoucherDto>();
+		
+		for ( int i = 0; i < voucherList.size(); i++ ) {
+			voucherDtoList.add( new VoucherDto( voucherList.get( i ) ) );
+		}
+		
+		return voucherDtoList;
+    }
 	
 	public boolean existsVoucher( String reference ) {
 		return this.voucherRepository.findByReference(reference) != null;
--- src/main/java/es/upm/miw/repositories/core/VoucherRepository.java
+++ src/main/java/es/upm/miw/repositories/core/VoucherRepository.java
@@ -4,8 +4,11 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import es.upm.miw.documents.core.Voucher;
+import es.upm.miw.dtos.UserMinimumDto;
+import es.upm.miw.dtos.VoucherDto;
 
 public interface VoucherRepository extends MongoRepository<Voucher, String> {
 	
