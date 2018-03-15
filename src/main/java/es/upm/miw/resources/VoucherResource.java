96880176c46c60534226ac29d792235c7e217359
angel <bo0477@192.168.7.19>
2018-03-15 15:21:53 +0100

    VoucherResource create & read

--- src/main/java/es/upm/miw/resources/VoucherResource.java
+++ src/main/java/es/upm/miw/resources/VoucherResource.java
@@ -3,9 +3,11 @@ package es.upm.miw.resources;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -16,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.upm.miw.controllers.VoucherController;
 import es.upm.miw.dtos.VoucherDto;
+import es.upm.miw.resources.exceptions.UserIdNotFoundException;
 import es.upm.miw.resources.exceptions.VoucherConsumedException;
 import es.upm.miw.resources.exceptions.VoucherReferenceNotFoundException;
 
@@ -29,33 +33,25 @@ public class VoucherResource {
 
     public static final String REFERENCE = "/{reference}";
     
-    /*@Autowired
-    private VoucherController voucherController;*/
+    @Autowired
+    private VoucherController voucherController;
     
     private VoucherDto voucher1 = new VoucherDto( "1", new BigDecimal(11) );
     
     @PostMapping
     public void createVoucher(@Valid @RequestBody VoucherDto voucherDto) {
-        //TODO
-    	// Empty for the mock
+    	
+    	if ( voucherDto.getValue() == null) {
+            voucherDto.setValue( new BigDecimal(11) );
+        }
+    	
+        this.voucherController.createVoucher( voucherDto );
     }
     
     @RequestMapping(value = REFERENCE, method = RequestMethod.GET)
     public VoucherDto readVoucher(@PathVariable String reference) throws VoucherReferenceNotFoundException {
     	
-    	VoucherDto voucherDto;
-    	
-    	/*if ( !this.voucherController.existsVoucher( reference ) ) {
-    		throw new VoucherReferenceNotFoundException();
-    	}*/
-    	
-    	if ( reference.equals( "1" ) ) {
-    		voucherDto = this.voucher1;
-    	}else {
-    		throw new VoucherReferenceNotFoundException();
-    	}
-    	
-        return voucherDto;
+    	return this.voucherController.readVoucher( reference ).orElseThrow(() -> new VoucherReferenceNotFoundException());
     }
     
     @PatchMapping(value = REFERENCE)
@@ -79,16 +75,8 @@ public class VoucherResource {
     @GetMapping
     public List<VoucherDto> readVoucherAll() {
     	
-    	List<VoucherDto> vouchersList = new ArrayList<VoucherDto>();
-        
-        VoucherDto voucher2 = new VoucherDto( "2", new BigDecimal(22) );
-        VoucherDto voucher3 = new VoucherDto( "3", new BigDecimal(33) );
-        
-        vouchersList.add(this.voucher1);
-        vouchersList.add(voucher2);
-        vouchersList.add(voucher3);
-        
-        return vouchersList;
+    	return this.voucherController.readVoucherAll();
+
     }
 
 }
