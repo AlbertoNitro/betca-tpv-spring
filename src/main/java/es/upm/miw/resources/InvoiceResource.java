84d068c44163e16347dd7b83bc0fa14eebb653e5
Juan Jimenez <jchantej@gmail.com>
2018-03-17 13:18:56 +0100

    Implementacion recurso y Test Generales

--- src/main/java/es/upm/miw/resources/InvoiceResource.java
+++ src/main/java/es/upm/miw/resources/InvoiceResource.java
@@ -1,5 +1,37 @@
 package es.upm.miw.resources;
 
-public class InvoiceResource {
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.upm.miw.controllers.InvoiceController;
+import es.upm.miw.dtos.TicketCreationInputDto;
+import es.upm.miw.resources.exceptions.FieldInvalidException;
+
+@PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('OPERATOR')")
+@RestController
+@RequestMapping(InvoiceResource.INVOICES)
+public class InvoiceResource {
+    public static final String INVOICES = "/invoices";
+
+    @Autowired
+    private InvoiceController invoiceController;
+
+    @PostMapping(produces = {"application/pdf", "application/json"})
+    public @ResponseBody byte[] createInvoice(@Valid @RequestBody TicketCreationInputDto ticketCreationDto) throws FieldInvalidException {
+        Optional<byte[]> pdf = this.invoiceController.createInvoice(ticketCreationDto);
+        if (!pdf.isPresent()) {
+            throw new FieldInvalidException("Article exception");
+        } else {
+            return pdf.get();
+        }
+    }
 }
