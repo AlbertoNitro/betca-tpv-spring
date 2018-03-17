923cf5b86cd2b18f049c6a3bee7d920fa0e8b538
Gabriel Munumel <gabrielmunumel@gmail.com>
2018-03-17 17:51:35 +0100

    Finalizada Tarea 5

--- src/main/java/es/upm/miw/documents/core/CashMovement.java
+++ src/main/java/es/upm/miw/documents/core/CashMovement.java
@@ -4,10 +4,7 @@ import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import es.upm.miw.utils.Encrypting;
 
 @Document
 public class CashMovement {
@@ -37,53 +34,10 @@ public class CashMovement {
         return creationDate;
     }
 
-    public Date getDateOfUse() {
-        return dateOfUse;
-    }
-
-    public void setDateOfUse(Date dateOfUse) {
-        this.dateOfUse = dateOfUse;
-    }
-
-    public void use() {
-        assert !this.isUsed();
-        dateOfUse = new Date();
-    }
-
-    public boolean isUsed() {
-        return dateOfUse != null;
-    }
-
-    @Override
-    public int hashCode() {
-        return reference.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        return reference.equals(((CashMovement) obj).reference);
-    }
-
     @Override
     public String toString() {
         String creationTime = new SimpleDateFormat("HH:00 dd-MMM-yyyy ").format(creationDate.getTime());
-        String useTime;
-        if (this.isUsed()) {
-            useTime = new SimpleDateFormat("HH:00 dd-MMM-yyyy ").format(dateOfUse.getTime());
-        } else {
-            useTime = "---";
-        }
-        return "CashMovement [reference=" + reference + ", value=" + value.doubleValue() + ", creationDate=" + creationTime + ", dateOfUse="
-                + useTime + "]";
+        return "CashMovement [value=" + value.doubleValue() + ", creationDate=" + creationTime + "]";
     }
 
 }
