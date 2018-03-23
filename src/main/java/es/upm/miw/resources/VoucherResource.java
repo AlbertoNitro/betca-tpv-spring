20387c7ab5b8f00d78bfb55ea6c06979a7d50ccd
angel <bo0477@192.168.7.19>
2018-03-23 17:13:33 +0100

    Remove readVoucher

--- src/main/java/es/upm/miw/resources/VoucherResource.java
+++ src/main/java/es/upm/miw/resources/VoucherResource.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,11 +43,6 @@ public class VoucherResource {
         } else {
             return pdf.get();
         }
-    }
-
-    @RequestMapping(value = REFERENCE, method = RequestMethod.GET)
-    public VoucherDto readVoucher(@PathVariable String reference) throws VoucherReferenceNotFoundException {
-        return this.voucherController.readVoucher(reference).orElseThrow(() -> new VoucherReferenceNotFoundException());
     }
 
     @PatchMapping(value = REFERENCE)
