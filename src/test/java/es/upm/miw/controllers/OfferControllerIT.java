7e76ada83956ab5c2771ac51d9e7961229382165
David.bv <david.bvt@gmail.com>
2018-03-26 11:49:39 +0200

    Done Offer Controller IT.

--- src/test/java/es/upm/miw/controllers/OfferControllerIT.java
+++ src/test/java/es/upm/miw/controllers/OfferControllerIT.java
@@ -3,12 +3,18 @@ package es.upm.miw.controllers;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import es.upm.miw.dtos.OfferInputDto;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,22 +25,30 @@ public class OfferControllerIT {
     private OfferController offerController;
 
     @Test
-    public void testCodeRepeatedFalse() {
-        assertFalse(true);
+    public void testCodeRepeatedFalse() throws ParseException {
+    	Date expiration = (new SimpleDateFormat("dd-MM-yyyy")).parse("01-12-2018");
+    	OfferInputDto offerInputDto = new OfferInputDto("CCC", new Float(0.33), expiration);
+        assertFalse(offerController.codeRepeated(offerInputDto));
     }
     
     @Test
-    public void testCodeRepeatedTrue() {
-        assertTrue(false);
+    public void testCodeRepeatedTrue() throws ParseException {
+    	Date expiration = (new SimpleDateFormat("dd-MM-yyyy")).parse("01-12-2018");
+    	OfferInputDto offerInputDto = new OfferInputDto("AAA", new Float(0.33), expiration);
+        assertTrue(offerController.codeRepeated(offerInputDto));
     }
     
     @Test
-    public void testIsExpirationTrue() {
-        assertFalse(true);
+    public void testIsExpirationTrue() throws ParseException {
+    	Date expiration = (new SimpleDateFormat("dd-MM-yyyy")).parse("01-12-2018");
+    	OfferInputDto offerInputDto = new OfferInputDto("CCC", new Float(0.33), expiration);
+        assertFalse(offerController.isExpiration(offerInputDto));
     }   
     
     @Test
-    public void testIsExpirationFalse() {
-        assertTrue(false);
+    public void testIsExpirationFalse() throws ParseException {
+    	Date expiration = (new SimpleDateFormat("dd-MM-yyyy")).parse("01-01-2018");
+    	OfferInputDto offerInputDto = new OfferInputDto("CCC", new Float(0.33), expiration);
+        assertTrue(offerController.codeRepeated(offerInputDto));
     }   
 }
