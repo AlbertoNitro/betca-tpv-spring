87e94c78ee91ad6c6368464065d3ebd9951fc752
David.bv <david.bvt@gmail.com>
2018-03-30 01:37:02 +0200

    Offer Resource is implemented.

--- src/main/java/es/upm/miw/resources/OfferResource.java
+++ src/main/java/es/upm/miw/resources/OfferResource.java
@@ -20,6 +20,7 @@ import es.upm.miw.controllers.OfferController;
 import es.upm.miw.dtos.OfferInputDto;
 import es.upm.miw.dtos.OfferOutputDto;
 import es.upm.miw.resources.exceptions.ForbiddenException;
+import es.upm.miw.resources.exceptions.OfferCodeNotFoundException;
 import es.upm.miw.resources.exceptions.OfferCodeRepeatedException;
 import es.upm.miw.resources.exceptions.OfferInvalidExpirationDateException;
 
@@ -27,33 +28,50 @@ import es.upm.miw.resources.exceptions.OfferInvalidExpirationDateException;
 @RestController
 @RequestMapping(OfferResource.OFFERS)
 public class OfferResource {
-
+	
     public static final String OFFERS = "/offers";
-
-    public static final String OFFER_ID = "/{id}";
+    public static final String OFFER_ID = "/{code}";
     
-
     @Autowired
     private OfferController offerController;
     
     @PostMapping
-    public void createOffer(@Valid @RequestBody OfferInputDto offerInputDto) throws OfferCodeRepeatedException, 
-      OfferInvalidExpirationDateException {
-    	throw new OfferCodeRepeatedException();
+    public void createOffer(@Valid @RequestBody OfferInputDto offerInputDto) 
+    		throws OfferCodeRepeatedException, OfferInvalidExpirationDateException {    	
+    	if (!offerController.codeRepeated(offerInputDto)) {
+    		throw new OfferInvalidExpirationDateException();
+    	}
+    	
+    	if (!offerController.isExpirationDateValid(offerInputDto)) {
+    		throw new OfferInvalidExpirationDateException();
+    	}
+    	offerController.createOffer(offerInputDto);
     }
 
     @PutMapping(value = OFFER_ID)
-    public void putOffer(@Valid @RequestBody OfferInputDto offerInputDto) throws OfferInvalidExpirationDateException {
-    	throw new OfferInvalidExpirationDateException();
+    public void putOffer(@Valid @RequestBody OfferInputDto offerInputDto) 
+    		throws OfferInvalidExpirationDateException, ForbiddenException, OfferInvalidExpirationDateException{
+    	if (!offerController.isExpirationDateValid(offerInputDto)) {
+    		throw new OfferInvalidExpirationDateException();
+    	}
+    	if (!offerController.codeRepeated(offerInputDto)) {
+    		throw new OfferInvalidExpirationDateException();
+    	}
+    	if (!offerController.putOffer(offerInputDto)) {
+    		throw new ForbiddenException(); 
+    	}
     }
 
     @DeleteMapping(value = OFFER_ID)
-    public void deleteOffer(@PathVariable String id) throws ForbiddenException {	
+    public void deleteOffer(@PathVariable String code) throws ForbiddenException {	
+    	if (!offerController.deleteOffer(code)) {
+    		throw new ForbiddenException(); 
+    	}
     }
 
     @RequestMapping(value = OFFER_ID, method = RequestMethod.GET)
-    public void readOffer(@PathVariable String id) throws ForbiddenException {
-    	 throw new ForbiddenException();
+    public OfferOutputDto readOffer(@PathVariable String code) throws OfferCodeNotFoundException {
+    	 return this.offerController.readOffer(code).orElseThrow(() -> new OfferCodeNotFoundException(code));
     }
 
     @GetMapping
