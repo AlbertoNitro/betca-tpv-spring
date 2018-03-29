8f3f46005b76c442996d92589770ec1f4210a459
David.bv <david.bvt@gmail.com>
2018-03-29 14:05:47 +0200

    Created Exception to Code repeated.

---
+++ src/main/java/es/upm/miw/resources/exceptions/OfferCodeRepeatedException.java
@@ -0,0 +1,15 @@
+package es.upm.miw.resources.exceptions;
+
+public class OfferCodeRepeatedException extends Exception {
+    private static final long serialVersionUID = -13446523452455385L;
+
+    public static final String DESCRIPTION = "Code of offer is repeated";
+
+    public OfferCodeRepeatedException() {
+        super(DESCRIPTION);
+    }
+
+    public OfferCodeRepeatedException(String detail) {
+        super(DESCRIPTION + ". " + detail);
+    }
+}
