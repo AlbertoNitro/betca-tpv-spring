5a1632cd4dc4a65d65149a3afcf90c0d2ddc1728
David.bv <david.bvt@gmail.com>
2018-03-30 14:29:47 +0200

    Implements news test on OfferResourceFunctionalTesting.

--- src/test/java/es/upm/miw/resources/OfferResourceFunctionalTesting.java
+++ src/test/java/es/upm/miw/resources/OfferResourceFunctionalTesting.java
@@ -3,15 +3,25 @@ package es.upm.miw.resources;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import es.upm.miw.documents.core.Offer;
 import es.upm.miw.dtos.OfferInputDto;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:test.properties")
 public class OfferResourceFunctionalTesting {
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
@@ -19,31 +29,33 @@ public class OfferResourceFunctionalTesting {
 	@Autowired
 	private RestService restService;
 	
-	private OfferInputDto offerInputDto;
-	
-	@Before
-	public void before() throws ParseException {
-		SimpleDateFormat dateFactory = new SimpleDateFormat("dd/MM/YYYY");
-	    this.offerInputDto = new OfferInputDto("ZZZ", new Float(0.5), dateFactory.parse("01/12/2018"));
-	}
+	private SimpleDateFormat factoryDate=  new SimpleDateFormat(Offer.DATE_FORMAT);
 	
     @Test
-    public void testCreateOffer() {
-    	
+    public void testCreateOffer() throws ParseException {
+    	OfferInputDto offerInputDto = new OfferInputDto("TEST-A", new Float(0.5), factoryDate.parse("2018-12-01"));
+    	this.restService.loginAdmin().restBuilder().path(OfferResource.OFFERS).body(offerInputDto).post().build();
     }
     
     @Test
-    public void testCreateOfferWithoutCodeException() {
-    	thrown.expect(new HttpMatcher(HttpStatus.BAD_REQUEST));
+    public void testCreateOfferWithoutCodeException() throws ParseException {
+    	this.thrown.expect(new HttpMatcher(HttpStatus.BAD_REQUEST));
+    	OfferInputDto offerInputDto = new OfferInputDto(null, new Float(0.5), factoryDate.parse("2018-12-01"));
+    	this.restService.loginAdmin().restBuilder().path(OfferResource.OFFERS).body(offerInputDto).post().build();
     }
     
     @Test
-    public void testCreateOfferWithoutPercentageException() {
-    	thrown.expect(new HttpMatcher(HttpStatus.BAD_REQUEST));
+    public void testCreateOfferWithoutPercentageException() throws ParseException {
+    	this.thrown.expect(new HttpMatcher(HttpStatus.BAD_REQUEST));
     }
     
     @Test
     public void testCreateOfferWithoutExpirationDateException() {
+    	thrown.expect(new HttpMatcher(HttpStatus.BAD_REQUEST));
+    }
+    
+    @Test
+    public void testCreateOfferWithExpirationDateThatIsExpiredException() {
     	thrown.expect(new HttpMatcher(HttpStatus.BAD_REQUEST));
     }
     
@@ -53,17 +65,12 @@ public class OfferResourceFunctionalTesting {
     }
     
     @Test
-    public void testPutOfferWithCodeThatNotExistException() {
+    public void testPutOfferWithThatNotExistException() {
         thrown.expect(new HttpMatcher(HttpStatus.NOT_FOUND));
     }
     
     @Test
     public void testReadOffer() {
 
-    }
-    
-    @Test
-    public void testReadOfferWithoutCodeThatNotExistException() {
-        thrown.expect(new HttpMatcher(HttpStatus.NOT_FOUND));
     }
 }
