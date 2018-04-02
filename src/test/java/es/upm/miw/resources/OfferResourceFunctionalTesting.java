403a119dbc0d556e6895c2e940592274e66885c5
David.bv <david.bvt@gmail.com>
2018-04-02 22:51:05 +0200

    Cleaning the code.

--- src/test/java/es/upm/miw/resources/OfferResourceFunctionalTesting.java
+++ src/test/java/es/upm/miw/resources/OfferResourceFunctionalTesting.java
@@ -4,7 +4,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
