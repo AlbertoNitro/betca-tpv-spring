dd56ef23342f1ac3b189aa04f2c77ba00d77ff7c
Jaime Hernandez <jaime.hernandezortiz@gmail.com>
2018-04-03 23:07:38 +0200

    Creación SchedulerControllerIT

---
+++ src/test/java/es/upm/miw/controllers/SchedulerControllerIT.java
@@ -0,0 +1,17 @@
+package es.upm.miw.controllers;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
+public class SchedulerControllerIT {
+
+    @Autowired
+    private SchedulerController schedulerController;
+
+}
