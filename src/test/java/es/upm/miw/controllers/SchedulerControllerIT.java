5ef5db20311ce6bd72f6ed37eaf0ab449482868e
Jaime Hernandez <jaime.hernandezortiz@gmail.com>
2018-04-03 23:45:43 +0200

    Generación de clases usando TDD.

--- src/test/java/es/upm/miw/controllers/SchedulerControllerIT.java
+++ src/test/java/es/upm/miw/controllers/SchedulerControllerIT.java
@@ -1,10 +1,15 @@
 package es.upm.miw.controllers;
 
+import es.upm.miw.dtos.SchedulerDto;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,4 +19,16 @@ public class SchedulerControllerIT {
     @Autowired
     private SchedulerController schedulerController;
 
+    private SchedulerDto schedulerDto;
+
+    @Before
+    public void before(){
+        this.schedulerDto = new SchedulerDto();
+    }
+
+    @Test
+    public void createProvider(){
+        //fail();
+
+    }
 }
