fc1e6cb883b43ab7139e8c1051987ca106b717a0
cova <webkova@gmail.com>
2018-04-05 09:08:39 +0200

    ReservationCreationInputDto creation

---
+++ src/main/java/es/upm/miw/dtos/ReservationCreationInputDto.java
@@ -0,0 +1,81 @@
+package es.upm.miw.dtos;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import es.upm.miw.dtos.validations.BigDecimalPositive;
+import es.upm.miw.dtos.validations.ListNotEmpty;
+
+public class ReservationCreationInputDto {
+	private String userMobile;
+
+    @BigDecimalPositive
+    private BigDecimal cash;
+
+    @BigDecimalPositive
+    private BigDecimal card;
+
+    @BigDecimalPositive
+    private BigDecimal voucher;
+
+    @ListNotEmpty
+    private List<ShoppingDto> shoppingCart;
+    
+    public ReservationCreationInputDto() {
+        // Empty for framework
+    }
+    
+    public ReservationCreationInputDto(BigDecimal cash, BigDecimal card, BigDecimal voucher, List<ShoppingDto> shoppingCart) {
+        this.cash = cash;
+        this.card = card;
+        this.voucher = voucher;
+        this.shoppingCart = shoppingCart;
+    }
+    
+    public String getUserMobile() {
+        return userMobile;
+    }
+
+    public void setUserMobile(String userMobile) {
+        this.userMobile = userMobile;
+    }
+
+    public BigDecimal getCash() {
+        return cash;
+    }
+
+    public void setCash(BigDecimal cash) {
+        this.cash = cash;
+    }
+
+    public BigDecimal getCard() {
+        return card;
+    }
+
+    public void setCard(BigDecimal card) {
+        this.card = card;
+    }
+
+    public BigDecimal getVoucher() {
+        return voucher;
+    }
+
+    public void setVoucher(BigDecimal voucher) {
+        this.voucher = voucher;
+    }
+
+    public List<ShoppingDto> getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(List<ShoppingDto> shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    @Override
+    public String toString() {
+        return "ReservationCreationDto [userMobile=" + userMobile + ", cash=" + cash + ", card=" + card + ", voucher=" + voucher
+                + ", shoppingCart=" + shoppingCart + "]";
+    }
+}
+
